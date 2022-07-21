8afd2c61c383169f1e43ff886d588ed76630427a
luantran <toilaluan@github.com>
2022-07-21 22:57:52 +0700

    sua solidArea

--- src/object/ObjInteraction.java
+++ src/object/ObjInteraction.java
@@ -18,7 +18,7 @@ public class ObjInteraction {
 						gp.obj.remove(gp.obj.get(i));
 //						gp.tileM.getMap("/maps/Rung.txt");
 						System.out.println("Move to next map!");
-						gp.tileM.updateMap("map/Map1.xml");
+						gp.tileM.updateMap("map/ice2layer.xml");
 					}
 					break;
 				case "HpPotion":
